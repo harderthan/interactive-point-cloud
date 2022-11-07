355dec8a6974928d3d282d4c40fed251c2f6b846
Harderthan <kheo1772@gmail.com>
2022-11-07 13:34:13 +0900

    fix CalculateCenter()

--- thirdparty/interactive_slam/glk/pointcloud_buffer.cpp
+++ thirdparty/interactive_slam/glk/pointcloud_buffer.cpp
@@ -1,26 +1,46 @@
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
 #include <glk/pointcloud_buffer.hpp>
 
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <pcl/point_cloud.h>
+namespace {
+
+Eigen::Vector3f CalculateCenter(
+    const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud) {
+  float x_min = std::numeric_limits<float>::max();
+  float x_max = std::numeric_limits<float>::max() * -1;
+  float y_min = std::numeric_limits<float>::max();
+  float y_max = std::numeric_limits<float>::max() * -1;
+  float z_min = std::numeric_limits<float>::max();
+  float z_max = std::numeric_limits<float>::max() * -1;
+  for (const auto& p : cloud->points) {
+    x_min = std::min(x_min, p.x);
+    x_max = std::max(x_max, p.x);
+    y_min = std::min(y_min, p.y);
+    y_max = std::max(y_max, p.y);
+    z_min = std::min(z_min, p.z);
+    z_max = std::max(z_max, p.z);
+  }
+  return Eigen::Vector3f((x_min + x_max) / 2.0f, (y_min + y_max) / 2.0f,
+                           (z_min + z_max) / 2.0f);
+}
+
+}  // namespace
 
 namespace glk {
 
 PointCloudBuffer::PointCloudBuffer(const std::string& cloud_filename) {
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
-  if(pcl::io::loadPCDFile(cloud_filename, *cloud)) {
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(
+      new pcl::PointCloud<pcl::PointXYZ>());
+  if (pcl::io::loadPCDFile(cloud_filename, *cloud)) {
     std::cerr << "error: failed to load " << cloud_filename << std::endl;
     num_points = 0;
     return;
   }
 
-  // Calculate the center of the point cloud.
-  Eigen::Vector3f point(0, 0, 0);
-  for (const auto& p : cloud->points) {
-    point += Eigen::Vector3f(p.x, p.y, p.z);
-  }
-  point /= cloud->size();
-  center = point;
+  center = CalculateCenter(cloud);
+  std::cout << center.transpose() << std::endl;
 
   stride = sizeof(pcl::PointXYZ);
   num_points = cloud->size();
@@ -32,20 +52,24 @@ PointCloudBuffer::PointCloudBuffer(const std::string& cloud_filename) {
 
   glGenBuffers(1, &vbo);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, cloud->size() * sizeof(pcl::PointXYZ), cloud->points.data(), GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, cloud->size() * sizeof(pcl::PointXYZ),
+               cloud->points.data(), GL_STATIC_DRAW);
 }
 
-PointCloudBuffer::PointCloudBuffer(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud) {
+PointCloudBuffer::PointCloudBuffer(
+    const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud) {
   stride = sizeof(pcl::PointXYZ);
   num_points = cloud->size();
-  std::cout << "num_points " << num_points << ", stride " << stride << std::endl;
+  std::cout << "num_points " << num_points << ", stride " << stride
+            << std::endl;
 
   glGenVertexArrays(1, &vao);
   glBindVertexArray(vao);
 
   glGenBuffers(1, &vbo);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, cloud->size() * sizeof(pcl::PointXYZ), cloud->points.data(), GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, cloud->size() * sizeof(pcl::PointXYZ),
+               cloud->points.data(), GL_STATIC_DRAW);
 }
 
 PointCloudBuffer::~PointCloudBuffer() {
@@ -54,7 +78,7 @@ PointCloudBuffer::~PointCloudBuffer() {
 }
 
 void PointCloudBuffer::draw(glk::GLSLShader& shader) {
-  if(num_points == 0) {
+  if (num_points == 0) {
     return;
   }
 
@@ -71,7 +95,5 @@ void PointCloudBuffer::draw(glk::GLSLShader& shader) {
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glDisableVertexAttribArray(0);
 }
-
-
 
 }  // namespace glk
